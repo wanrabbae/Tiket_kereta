cd4a569b8902fba30f5095655f3a422245b0b9da
wanrabbae <wancode.msg@gmail.com>
2023-02-16 19:57:42 +0700

    ENH API MOBILE

--- backend_kai/app/Http/Controllers/TrainApiCtrl.php
+++ backend_kai/app/Http/Controllers/TrainApiCtrl.php
@@ -13,17 +13,35 @@ class TrainApiCtrl extends Controller
 {
     public function getStations(Request $request)
     {
-        response()->json(TrainStation::all());
+        $data = TrainStation::orderBy('id', 'DESC')->get(['id', 'station_code', 'station_name', 'city']);
+        return response()->json($data);
     }
 
     public function findTicket(Request $request)
     {
         $data = [];
         if ($request->has('depart') && $request->has('arrival')) {
-            $data = TrainJourney::with(['train', 'train_fare', 'train_station_depart', 'train_station_arrival'])->where('train_station_depart.station_name', $request->query('depart'))->where('train_station_arrival.station_name', $request->query('arrival'))->get();
+            $data = TrainJourney::with(['train', 'train_station_depart', 'train_station_arrival'])->whereRelation('train_station_depart', 'station_name', '=', $request->query('depart'))->whereRelation('train_station_arrival', 'station_name', '=', $request->query('arrival'))->get();
+
+            if ($data) {
+                foreach ($data as $key) {
+                    $fare = TrainFare::where('train_no', $key['train_no'])->first();
+                    if ($fare) {
+                        $key["train_fare"] = $fare;
+                    }
+                }
+            }
         } else {
-            $data = TrainJourney::with(['train', 'train_fare', 'train_station_depart', 'train_station_arrival'])->get();
+            $data = TrainJourney::with(['train', 'train_station_depart', 'train_station_arrival'])->get();
+            if ($data) {
+                foreach ($data as $key) {
+                    $fare = TrainFare::where('train_no', $key['train_no'])->first();
+                    if ($fare) {
+                        $key["train_fare"] = $fare;
+                    }
+                }
+            }
         }
-        response()->json($data);
+        return response()->json($data);
     }
 }
