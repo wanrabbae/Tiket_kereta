b295e4a2d62257810b911be9e3118d849088de05
wanrabbae <wancode.msg@gmail.com>
2023-02-23 15:10:46 +0700

    ENH Screen order ticket and Feat pilih kursi screen

--- mobile_kai/lib/screens/ticket_order_screen.dart
+++ mobile_kai/lib/screens/ticket_order_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:kai_mobile/core/utils/navigator_helper.dart';
+import 'package:kai_mobile/screens/book_seat_screen.dart';
 import 'package:kai_mobile/utils/app_styles.dart';
 
 class TicketOrder extends StatefulWidget {
@@ -93,12 +94,116 @@ class _TicketOrderState extends State<TicketOrder> {
         child: Column(children: [
           Container(
             padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(color: Colors.white),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: getInputPsg(),
             ),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Kereta",
+                  style: Styles.headLineStyle2.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("AWN - JNG   |   Rab, 30 Feb 2022   |   13:31"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Harga",
+                  style: Styles.headLineStyle2.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Pergi",
+                  style: Styles.headLineStyle3.copyWith(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Penumpang x" +
+                        widget.dataJourney!["passengerCount"].toString()),
+                    Text("IDR. 500.000")
+                  ],
+                ),
+                Divider(
+                  color: Colors.grey.shade600,
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Total Pembayaran",
+                      style: Styles.headLineStyle1
+                          .copyWith(color: Colors.black, fontSize: 15),
+                    ),
+                    Text(
+                      "IDR. 500.000",
+                      style: Styles.headLineStyle1
+                          .copyWith(color: Styles.primaryBold, fontSize: 18),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onTap: () {
+              goPush(BookSeat(), context);
+            },
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 15),
+              decoration: BoxDecoration(
+                color: Styles.primaryBold,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                  child: Text(
+                "Pilih Kursi",
+                style: Styles.headLineStyle3.copyWith(color: Colors.white),
+              )),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
         ]),
       )),
     );
